40dc01be8f2cab2ed3c9d139ffd409ae70a2fd3d
Ivan <jesus.bta.01@gmail.com>
2023-03-23 01:21:24 -0600

    update

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ const requireToken = require('./Middlewares/AuthTokenRequired');
 app.use(bodyParser.json());
 app.use(authRoutes);
 //
-app.get('/', (req,res)=>{
-    res.send('Bienvenido ami api');
-});
 
+app.get('/',requireToken,  (req, res) => {
+    
+    console.log(req.user);
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
